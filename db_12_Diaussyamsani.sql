d0f67a430cddbffb4af548edaf1192a7f7b12d58
Dyah <88519281+Diaussyamsani@users.noreply.github.com>
2021-08-12 10:25:26 +0700

    Update db_12_Diaussyamsani.sql

--- db_12_Diaussyamsani.sql
+++ db_12_Diaussyamsani.sql
@@ -1,39 +1,39 @@
 -- phpMyAdmin SQL Dump
--- versi 4.8.5
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Tuan rumah: 127.0.0.1
--- Waktu Pembuatan: 08 Agustus 2021 pukul 06:32
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 7.3.2
+-- Host: 127.0.0.1
+-- Generation Time: Aug 05, 2021 at 09:12 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
-MULAI TRANSAKSI;
-SET zona_waktu = "+00:00";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMA utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Basis data: `db_12_Diaussyamsani.sql`
+-- Database: `db_12_Diaussyamsani.sql`
 --
 
--------------------------------------------------- --------
+-- --------------------------------------------------------
 
 --
--- Struktur tabel untuk tabel `db_12_Diaussyamsani.sql`
+-- Table structure for table `db_12_Diaussyamsani.sql`
 --
 
-BUAT TABEL `db_12_Diaussyamsani.sql` (
-  `no absen` int(5) TIDAK NULL,
-  `nama` varchar(100) TIDAK NULL,
-  `tidak ada hp` int(15) TIDAK NULL
+CREATE TABLE `db_12_Diaussyamsani.sql` (
+  `no absen` int(5) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `no hp` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-MELAKUKAN;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
